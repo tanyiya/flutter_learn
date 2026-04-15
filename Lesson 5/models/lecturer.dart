353314c38c7922d75ff99b6432dbee9bc52e4b55
Yi Ya <tanyiya04@gmail.com>
2026-04-15 23:45:00 +0800

    Add files via upload

---
+++ Lesson 5/models/lecturer.dart
@@ -0,0 +1,11 @@
+class Lecturer {
+  String name;
+  String employeeId;
+  String department;
+
+  Lecturer({
+    required this.name,
+    required this.employeeId,
+    required this.department,
+  });
+}
---
+++ Lesson 5/models/student.dart
@@ -0,0 +1,11 @@
+class Student {
+  String name;
+  String matricNo;
+  String course;
+
+  Student({
+    required this.name,
+    required this.matricNo,
+    required this.course,
+  });
+}
